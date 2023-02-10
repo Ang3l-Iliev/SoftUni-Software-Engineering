119829d748aef2b6e3596d735a672d7dbc3a2759
Angel Iliev <113524377+Ang3l-Iliev@users.noreply.github.com>
2023-02-10 17:22:49 +0200

    Create nxnMatrix.js

---
+++ JS-Fundamental/Functions/nxnMatrix.js
@@ -0,0 +1,18 @@
+function matrix(times) {
+  let matrix = [];
+
+  for (let i = 0; i < times; i++) {
+
+    let row = [];
+
+    for (let j = 0; j < times; j++) {
+      row.push(times);
+      
+    }
+   matrix.push(row.join(" "))
+  }
+
+console.log(matrix.join('\n'));
+}
+
+matrix(3);
