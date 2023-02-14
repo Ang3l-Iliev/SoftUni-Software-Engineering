47d760cb5bbf55d6215d4932d7fd5f1e2e0159ca
Angel Iliev <113524377+Ang3l-Iliev@users.noreply.github.com>
2023-02-14 11:39:17 +0200

    Create app.js

---
+++ JS-Advanced/DOM Manipulations and Events/List-Of-Items/app.js
@@ -0,0 +1,11 @@
+function addItem() {
+    let inputElement = document.getElementById('newItemText')
+    let itemsElement=document.getElementById('items')
+    
+    let liElement=document.createElement('li')
+    liElement.textContent=inputElement.value
+
+    itemsElement.appendChild(liElement)
+    inputElement.value =" "
+
+}
