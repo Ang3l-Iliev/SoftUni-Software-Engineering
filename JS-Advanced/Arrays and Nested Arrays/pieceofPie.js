9d4f0b28a0f14941457462448a2ba91992492a18
Angel Iliev <113524377+Ang3l-Iliev@users.noreply.github.com>
2023-02-14 11:27:01 +0200

    Create pieceofPie.js

---
+++ JS-Advanced/Arrays and Nested Arrays/pieceofPie.js
@@ -0,0 +1,13 @@
+function solve(pies, startFlavor, endFlavor) {
+    const start = pies.indexOf(startFlavor);
+    const end = pies.indexOf(endFlavor)+1;
+    const result = pies.slice(start, end);
+    console.log(result); 
+}
+
+
+solve(['Pumpkin Pie','Key Lime Pie','Cherry Pie','Lemon Meringue Pie','Sugar Cream Pie'],
+
+    'Key Lime Pie',
+
+    'Lemon Meringue Pie')
