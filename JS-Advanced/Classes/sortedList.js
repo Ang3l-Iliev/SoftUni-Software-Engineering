954165091e44ff7499101fdd79c5dc3480d531b1
Angel Iliev <113524377+Ang3l-Iliev@users.noreply.github.com>
2023-02-14 11:33:59 +0200

    Create sortedList.js

---
+++ JS-Advanced/Classes/sortedList.js
@@ -0,0 +1,32 @@
+class List {
+    constructor() {
+        this.result=[]
+        this.size = this.result.length
+    }
+
+    add(value) {
+        this.result.push(value)
+        this.result = this.result.length
+        this.result.sort((a, b) => a - b)
+    }
+    remove(index) {
+        if (index < 0 || index >= this.result.length) {
+            throw new Error('Invalid Index')
+        }
+        this.result.splice(index, 1)
+        this.size = this.result.length
+
+    }
+    get(index) {
+        if (index < 0 || index >= this.result.length) {
+            throw new Error('Invalid Index')
+        }
+        return this.result[index]
+    }
+}
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1)); 
+
