1924b3b7be66c3599bd2bf5eead60da75d02187e
Angel Iliev <113524377+Ang3l-Iliev@users.noreply.github.com>
2023-02-14 11:26:45 +0200

    Create biggestElement.js

---
+++ JS-Advanced/Arrays and Nested Arrays/biggestElement.js
@@ -0,0 +1,13 @@
+function biggestElement(...arr) {
+    // let firstArr = arr[0]
+    // let secondArr = arr[1]
+    // let finalArr = firstArr.concat(secondArr)
+    // console.log(Math.max(...finalArr));
+
+
+
+}
+
+
+biggestElement([[20, 50, 10],
+[8, 33, 145]])
